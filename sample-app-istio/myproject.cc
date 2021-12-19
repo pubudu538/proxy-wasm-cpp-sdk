d8f41c0bb26cb43a7198fb1918574a57e6b65c76
pubudu538 <pubudu538@gmail.com>
2021-12-19 19:32:46 +0530

    Sun Dec 19 19:32:46 +0530 2021

--- sample-app-istio/myproject.cc
+++ sample-app-istio/myproject.cc
@@ -3,12 +3,16 @@
 
 #include "proxy_wasm_intrinsics.h"
 
+using echo::Config;
+
 class ExampleRootContext : public RootContext {
 public:
   explicit ExampleRootContext(uint32_t id, std::string_view root_id) : RootContext(id, root_id) {}
 
   bool onStart(size_t) override;
   bool onConfigure(size_t) override;
+
+  echo::Config config_;
 };
 
 class ExampleContext : public Context {
@@ -18,10 +22,6 @@ public:
   FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
   void onDone() override;
 
-// private:
-//   inline ExampleRootContext* rootContext() {
-//     return dynamic_cast<ExampleRootContext*>(this->root());
-//   }
 };
 static RegisterContextFactory register_ExampleContext(CONTEXT_FACTORY(ExampleContext),
                                                         ROOT_FACTORY(ExampleRootContext));
@@ -42,8 +42,27 @@ bool ExampleRootContext::onStart(size_t) {
   return true;
 }
 
-bool ExampleRootContext::onConfigure(size_t) {
+// bool ExampleRootContext::onConfigure(size_t) {
+//   logInfo("onConfigure");
+//   proxy_set_tick_period_milliseconds(1000); // 1 sec
+//   return true;
+// }
+
+bool ExampleRootContext::onConfigure(size_t config_size) {
+  LOG_INFO("onConfigure called");
   logInfo("onConfigure");
   proxy_set_tick_period_milliseconds(1000); // 1 sec
+  const WasmDataPtr configuration = getBufferBytes(WasmBufferType::PluginConfiguration, 0, config_size);
+
+    JsonParseOptions json_options;
+    const Status options_status = JsonStringToMessage(
+        configuration->toString(),
+        &config_,
+        json_options);
+    if (options_status != Status::OK) {
+      LOG_WARN("Cannot parse plugin configuration JSON string: " + configuration->toString());
+      return false;
+    }
+    LOG_INFO("Loading Config: " + config_.clustername());
   return true;
 }
