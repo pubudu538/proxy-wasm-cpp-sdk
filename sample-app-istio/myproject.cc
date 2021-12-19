17e770b4e8acc028842706597e43f32e338bfbc0
pubudu538 <pubudu538@gmail.com>
2021-12-19 12:48:02 +0530

    Sun Dec 19 12:48:02 +0530 2021

--- sample-app-istio/myproject.cc
+++ sample-app-istio/myproject.cc
@@ -1,5 +1,4 @@
 #include <string>
-#include <string_view>
 #include <unordered_map>
 
 #include "proxy_wasm_intrinsics.h"
@@ -9,90 +8,28 @@ public:
   explicit ExampleRootContext(uint32_t id, std::string_view root_id) : RootContext(id, root_id) {}
 
   bool onStart(size_t) override;
-  bool onConfigure(size_t) override;
-  void onTick() override;
 };
 
 class ExampleContext : public Context {
 public:
-  explicit ExampleContext(uint32_t id, RootContext *root) : Context(id, root) {}
+  explicit ExampleContext(uint32_t id, RootContext* root) : Context(id, root) {}
 
-  void onCreate() override;
   FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
-  FilterDataStatus onRequestBody(size_t body_buffer_length, bool end_of_stream) override;
-  FilterHeadersStatus onResponseHeaders(uint32_t headers, bool end_of_stream) override;
-  FilterDataStatus onResponseBody(size_t body_buffer_length, bool end_of_stream) override;
   void onDone() override;
-  void onLog() override;
-  void onDelete() override;
-
-private:
- inline ExampleRootContext* rootContext() {
-  return dynamic_cast<ExampleRootContext*>(this->root());
-}
 };
-static RegisterContextFactory register_ExampleContext(CONTEXT_FACTORY(ExampleContext),
-                                                      ROOT_FACTORY(ExampleRootContext),
-                                                      "my_root_id");
+static RegisterContextFactory register_ExampleContext(CONTEXT_FACTORY(ExampleContext));
+
+FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t headers, bool end_of_stream) {
+  logInfo(std::string("onRequestHeaders ") + std::to_string(id()));
+  auto path = getRequestHeader(":path");
+  logInfo(std::string("header path ") + std::string(path->view()));
+  addResponseHeader("X-Wasmss-custom", "FOO");
+  return FilterHeadersStatus::Continue;
+}
+
+void ExampleContext::onDone() { logInfo("onDone " + std::to_string(id())); }
 
 bool ExampleRootContext::onStart(size_t) {
-  LOG_INFO("onStart");
+  LOG_TRACE("onStart");
   return true;
 }
-
-bool ExampleRootContext::onConfigure(size_t) {
-  LOG_INFO("onConfigure");
-  logInfo("onConfigure");
-  proxy_set_tick_period_milliseconds(1000); // 1 sec
-  return true;
-}
-
-void ExampleRootContext::onTick() { LOG_TRACE("onTick"); }
-
-void ExampleContext::onCreate() { LOG_WARN(std::string("onCreate " + std::to_string(id()))); }
-
-FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t, bool) {
-  logInfo("ssssss");
-  LOG_INFO(std::string("onRequestHeaders ") + std::to_string(id()));
-  logInfo(std::string("onRequestHeaders ") + std::to_string(id()));
-  auto result = getRequestHeaderPairs();
-  auto pairs = result->pairs();
-  LOG_INFO(std::string("headers: ") + std::to_string(pairs.size()));
-  for (auto &p : pairs) {
-    LOG_INFO(std::string(p.first) + std::string(" -> ") + std::string(p.second));
-  }
-  return FilterHeadersStatus::Continue;
-}
-
-FilterHeadersStatus ExampleContext::onResponseHeaders(uint32_t, bool) {
-  LOG_DEBUG(std::string("onResponseHeaders ") + std::to_string(id()));
-  auto result = getResponseHeaderPairs();
-  auto pairs = result->pairs();
-  LOG_INFO(std::string("headers: ") + std::to_string(pairs.size()));
-  for (auto &p : pairs) {
-    LOG_INFO(std::string(p.first) + std::string(" -> ") + std::string(p.second));
-  }
-  addResponseHeader("X-Wasm-custom", "FOO");
-  replaceResponseHeader("content-type", "text/plain; charset=utf-8");
-  removeResponseHeader("content-length");
-  return FilterHeadersStatus::Continue;
-}
-
-FilterDataStatus ExampleContext::onRequestBody(size_t body_buffer_length,
-                                               bool /* end_of_stream */) {
-  auto body = getBufferBytes(WasmBufferType::HttpRequestBody, 0, body_buffer_length);
-  LOG_ERROR(std::string("onRequestBody ") + std::string(body->view()));
-  return FilterDataStatus::Continue;
-}
-
-FilterDataStatus ExampleContext::onResponseBody(size_t /* body_buffer_length */,
-                                                bool /* end_of_stream */) {
-  setBuffer(WasmBufferType::HttpResponseBody, 0, 12, "Hello, world");
-  return FilterDataStatus::Continue;
-}
-
-void ExampleContext::onDone() { LOG_WARN(std::string("onDone " + std::to_string(id()))); }
-
-void ExampleContext::onLog() { LOG_WARN(std::string("onLog " + std::to_string(id()))); }
-
-void ExampleContext::onDelete() { LOG_WARN(std::string("onDelete " + std::to_string(id()))); }
