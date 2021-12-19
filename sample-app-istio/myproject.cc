cc4cd9b12dcde8db12bbb3e85a81eb3fc5fe8f12
pubudu538 <pubudu538@gmail.com>
2021-12-19 19:45:58 +0530

    Sun Dec 19 19:45:58 +0530 2021

--- sample-app-istio/myproject.cc
+++ sample-app-istio/myproject.cc
@@ -1,13 +1,7 @@
 #include <string>
-#include <string_view>
 #include <unordered_map>
 
 #include "proxy_wasm_intrinsics.h"
-
-#include "google/protobuf/util/json_util.h"
-
-using google::protobuf::util::JsonParseOptions;
-
 
 class ExampleRootContext : public RootContext {
 public:
@@ -15,21 +9,17 @@ public:
 
   bool onStart(size_t) override;
   bool onConfigure(size_t) override;
-
-  echo::Config config_;
 };
 
 class ExampleContext : public Context {
 public:
-  explicit ExampleContext(uint32_t id, RootContext* root) : Context(id, root) {}
+  explicit ExampleContext(uint32_t id, RootContext *root) : Context(id, root) {}
 
   FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
   void onDone() override;
-
 };
 static RegisterContextFactory register_ExampleContext(CONTEXT_FACTORY(ExampleContext),
-                                                        ROOT_FACTORY(ExampleRootContext));
-
+                                                      ROOT_FACTORY(ExampleRootContext));
 
 FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t headers, bool end_of_stream) {
   logInfo(std::string("onRequestHeaders ") + std::to_string(id()));
@@ -55,18 +45,20 @@ bool ExampleRootContext::onStart(size_t) {
 bool ExampleRootContext::onConfigure(size_t config_size) {
   LOG_INFO("onConfigure called");
   logInfo("onConfigure");
-  proxy_set_tick_period_milliseconds(1000); // 1 sec
-  const WasmDataPtr configuration = getBufferBytes(WasmBufferType::PluginConfiguration, 0, config_size);
+  // proxy_set_tick_period_milliseconds(1000); // 1 sec
 
-    JsonParseOptions json_options;
-    const Status options_status = JsonStringToMessage(
-        configuration->toString(),
-        &config_,
-        json_options);
-    if (options_status != Status::OK) {
-      LOG_WARN("Cannot parse plugin configuration JSON string: " + configuration->toString());
-      return false;
-    }
-    LOG_INFO("Loading Config: " + config_.clustername());
+  auto configuration_data =
+      getBufferBytes(WasmBufferType::PluginConfiguration, 0, configuration_size);
+  // Parse configuration JSON string.
+  auto result = ::Wasm::Common::JsonParse(configuration_data->view());
+  if (!result.has_value()) {
+    LOG_WARN(absl::StrCat("cannot parse plugin configuration JSON string: ",
+                          configuration_data->view()));
+    return false;
+  } else {
+    logInfo("onConfigure..........$$$$$");
+    LOG_INFO(
+        absl::StrCat("can parse plugin configuration JSON string: ", configuration_data->view()));
+  }
   return true;
 }
